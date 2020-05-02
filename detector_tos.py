5e6be0de6cc7240a8ecf2e4907686b4a44c6c992
Emanuel Estrada Larios <emamex98@gmail.com>
2020-05-01 19:41:07 -0500

    Fixed confusion matrix

--- detector_tos.py
+++ detector_tos.py
@@ -28,36 +28,31 @@ class HMMTrainer(object):
   def get_score(self, input_data):
     return self.model.score(input_data)
 
-def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
+def plot_confusion_matrix():
 
-    if normalize:
-        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        print("Normalized confusion matrix")
-    else:
-        print('Confusion matrix, without normalization')
+    classes = ["Cough","No Cough"]
 
-    print(cm)
-
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
+    plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
     plt.colorbar()
+
+    plt.title("Normalized Confusion Matrix")
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
+    plt.xticks(tick_marks, classes)
     plt.yticks(tick_marks, classes)
 
-    fmt = '.2f' if normalize else 'd'
-    thresh = cm.max() / 2.
-    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt),
-                 horizontalalignment="center",
-                 color="white" if cm[i, j] > thresh else "black")
+    plt.ylabel('True')
+    plt.xlabel('Predicted')
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    for i in range(2):
+        for j in range(2):
+            c = round(conf_matrix[j,i], 5)
+            plt.text(i, j, str(c), va='center', ha='center')
+
+    plt.show()
+
 
 hmm_models = []
-input_folder = 'C:\\Users\\max29\\Downloads\\tos\\entrenamiento'
+input_folder = '../detector-de-tos-markov/entrenamiento'
 
 X = np.array([])
 
@@ -81,13 +76,17 @@ print("Start training")
 hmm_trainer.train(X)
 print("Finish training")
 
-
-
-input_folder_tos = './detector-de-tos-markov'
-
+input_folder_tos = '../detector-de-tos-markov/prueba'
 
 real = []
 predicted = []
+
+TP = 0
+FP = 0
+FN = 0
+TN = 0
+
+n = 0
 
 max_score = -9999999999999999999
 
@@ -109,21 +108,37 @@ for dir in os.listdir(input_folder_tos):
         mfcc_features = mfcc(audio, sampling_freq, nfft=1200)
         score = hmm_trainer.get_score(mfcc_features)
         if dir == "notos":
-            print(score)
             real_output = "No Cough"
         else:
             real_output = "Cough"
+
         output = "Cough"
+
         if (score * 100) / max_score > 55:
             max_score = score
             output = "No Cough"
         predicted.append(output)
         real.append(real_output)
 
-cm = confusion_matrix(real, predicted)
-np.set_printoptions(precision=2)
-classes = ["Cough","No Cough"]
-plt.figure()
-plot_confusion_matrix(cm, classes=classes, normalize=True, title='Normalized confusion matrix')
+        n += 1
 
-plt.show()
+        print("\nPREDICTED::", output)
+        print("ACTUAL_VALUE:", real_output)
+
+        if output == "Cough" and real_output == "Cough":
+            TP += 1
+            print("-> TRUE POSITIVE")
+        elif output == "Cough" and real_output == "No Cough":
+            FN += 1
+            print("-> FALSE NEGATIVE")
+        elif output == "No Cough" and real_output == "No Cough":
+            TN += 1
+            print("-> TRUE NEGATIVE")
+        elif output == "No Cough" and real_output == "Cough":
+            FP += 1
+            print("-> FALSE POSITIVE")
+
+
+conf_matrix = np.matrix([[TP/n, FP/n], [FN/n, TN/n]])
+print("\n", conf_matrix)
+plot_confusion_matrix()
